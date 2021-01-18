0796c46d197df1f568e1099348514fdd3937cfc4
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-01-19 05:03:38 +0900

    matrixできた

--- src/titechWeb.rb
+++ src/titechWeb.rb
@@ -8,28 +8,31 @@ class TitechWeb
       driver = Selenium::WebDriver.for :chrome
       driver.get LOGIN_URL
       login_info = LoginInfo.new()
-      element_username = driver.find_element(:name, 'usr_name')
-      element_username.send_keys login_info.username
-      element_password = driver.find_element(:name, 'usr_password')
-      element_password.send_keys login_info.password
-      element_ok = driver.find_element(:name, 'OK')
-      element_ok.click
+      driver.find_element(:name, 'usr_name').send_keys login_info.username
+      driver.find_element(:name, 'usr_password').send_keys login_info.password
+      driver.find_element(:name, 'OK').click
 
-      sleep 2
+      sleep 1
 
       el_pass1 = driver.find_element(:xpath, '//*[@id="authentication"]/tbody/tr[4]/th[1]').text
       el_pass2 = driver.find_element(:xpath, '//*[@id="authentication"]/tbody/tr[5]/th[1]').text
       el_pass3 = driver.find_element(:xpath, '//*[@id="authentication"]/tbody/tr[6]/th[1]').text
       input_pass1 = driver.find_element(:name, 'message3')
-      input_pass1.send_keys login_info.matrix[el_pass1[1]][el_pass1[3]]
+      print(login_info.matrix[el_pass1[1]][el_pass1[3].to_i - 1])
+      input_pass1.send_keys login_info.matrix[el_pass1[1]][el_pass1[3].to_i - 1]
       input_pass2 = driver.find_element(:name, 'message4')
-      input_pass2.send_keys login_info.matrix[el_pass2[1]][el_pass1[3]]
+      print(login_info.matrix[el_pass2[1]][el_pass2[3].to_i - 1])
+      input_pass2.send_keys login_info.matrix[el_pass2[1]][el_pass1[3].to_i - 1]
       input_pass3 = driver.find_element(:name, 'message5')
-      input_pass3.send_keys login_info.matrix[el_pass3[1]][el_pass3[3]]
+      print(login_info.matrix[el_pass3[1]][el_pass3[3].to_i - 1])
+      input_pass3.send_keys login_info.matrix[el_pass3[1]][el_pass3[3].to_i - 1]
+      sleep 20
       element_login = driver.find_element(:name, 'OK')
       element_login.click
+
+      sleep 1
     ensure
-      os.kill(driver.service.process.pid, signal.SIGTERM)
+      sleep 1
     end
   end
 end
@@ -37,8 +40,7 @@ end
 
 if __FILE__ == $0
   titech = TitechWeb.new()
-  # titech.login()
+  titech.login()
   login_info = LoginInfo.new()
-  print(login_info.matrix[:"A"])
 end
 
